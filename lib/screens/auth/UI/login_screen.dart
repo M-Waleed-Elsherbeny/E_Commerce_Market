10c90ca5e26b8a2ba5a7bb5630081c6da3eaf2ae
M-Waleed-Elsherbeny <m.elsherbeny100@gmail.com>
2025-06-23 00:46:23 +0300

    Preparing login Screen UI

--- lib/screens/auth/UI/login_screen.dart
+++ lib/screens/auth/UI/login_screen.dart
@@ -9,8 +9,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  double? deviceWidth, deviceHeight;
   @override
   Widget build(BuildContext context) {
+    deviceWidth = MediaQuery.of(context).size.width;
+    deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -20,25 +23,119 @@ class _LoginScreenState extends State<LoginScreen> {
                 "Welcome To Our Market",
                 style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
               ),
-              Card(
-                elevation: 10,
-                margin: EdgeInsets.all(20),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                decoration: BoxDecoration(
+                  color: AppColors.kWhiteColor,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(color: AppColors.kGreyColor, blurRadius: 5),
+                  ],
+                ),
+                padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+                height: deviceHeight! * 0.8,
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
                   children: [
                     CustomTextFormField(
                       labelText: "Email",
                       prefixIcon: IconButton(
                         onPressed: () {},
-                        icon: Icon(Icons.email),
+                        icon: Icon(
+                          Icons.email_outlined,
+                          color: AppColors.kPrimaryColor,
+                        ),
                       ),
                     ),
-                    SizedBox(height: 15),
+                    SizedBox(height: 20),
                     CustomTextFormField(
                       labelText: "Password",
                       prefixIcon: IconButton(
                         onPressed: () {},
-                        icon: Icon(Icons.lock),
+                        icon: Icon(
+                          Icons.lock_outline,
+                          color: AppColors.kPrimaryColor,
+                        ),
                       ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Forget Password?",
+                          style: TextStyle(
+                            color: AppColors.kPrimaryColor,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: deviceHeight! * 0.05),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(deviceWidth!, deviceHeight! * 0.05),
+                        backgroundColor: AppColors.kPrimaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.kWhiteColor,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: deviceHeight! * 0.05),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          width: deviceWidth! * 0.25,
+                          child: Divider(
+                            color: AppColors.kGreyColor,
+                            thickness: 2,
+                          ),
+                        ),
+                        Text(
+                          "Or Login With",
+                          style: TextStyle(
+                            color: AppColors.kBlackColor,
+                            fontSize: 15,
+                          ),
+                        ),
+                        SizedBox(
+                          width: deviceWidth! * 0.25,
+                          child: Divider(
+                            color: AppColors.kGreyColor,
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                    // SizedBox(height: deviceHeight! * 0.05),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don't have an account?"),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Register Now",
+                            style: TextStyle(
+                              color: AppColors.kPrimaryColor,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
@@ -77,7 +174,7 @@ class CustomTextFormField extends StatelessWidget {
         prefixIcon: prefixIcon,
         label: Text(
           labelText,
-          style: TextStyle(fontSize: 15, color: AppColors.kPrimaryColor),
+          style: TextStyle(fontSize: 15, color: AppColors.kGreyColor),
         ),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
@@ -99,3 +196,6 @@ class CustomTextFormField extends StatelessWidget {
     );
   }
 }
+
+
+// Preparing login Screen UI
